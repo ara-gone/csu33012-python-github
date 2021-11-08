1acfad7f6f5d56714284123bc660ebf2ad6a48a2
Stephen Barrett <stephen.barrett@tcd.ie>
2021-11-08 11:51:18 +0000

    install pip and build some basic data access

--- script.py
+++ script.py
@@ -1,5 +1,23 @@
 
 # a script to do python based access to the github api
-# let's start with a simple hellow world app to get outselves going.
+# step 2 - let's get PyGitHub going.
+# we have to pip install PyGithub on the command line.
+
 print("Demonstration python based github api access");
+
+#import Github from the PyGithub library
+from github import Github
+
+#we initialise a PyGithub Github object with our access token.
+# note that this token is ours, and now deleted. You must crete your own access
+# token and use here instead. 
+g = Github("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")
+
+#Let's get the user object and print some trivial details
+usr = g.get_user()
+print("user:     " + usr.login)
+print("fullname: " + usr.name)
+print("location: " + usr.location)
+print("company:  " + usr.company)
+
 
