41561dd1f1de78b2e5e051255d2b164776893731
Stephen Barrett <stephen.barrett@tcd.ie>
2021-11-08 12:14:14 +0000

    Extract data into a dictonary for subsequent storage

--- script.py
+++ script.py
@@ -1,28 +1,28 @@
 
 # a script to do python based access to the github api
-# step 3 - let's fix return type handling
+# step 3 - Let's create a data dictionary for storage
 
 print("Demonstration python based github api access");
 
 #import Github from the PyGithub library
 from github import Github
+import json # for dictionary to string
 
 #we initialise a PyGithub Github object with our access token.
 # note that this token is ours, and now deleted. You must crete your own access
 # token and use here instead. 
 g = Github("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")
 
-#Let's get the user object and print some trivial details
+#Let's get the user object and build a data dictionary
 usr = g.get_user()
-print("user:     " + usr.login)
 
-if usr.name is not None:
-    print("fullname: " + usr.name)
-    
-if usr.location is not None:    
-    print("location: " + usr.location)
-    
-if usr.company is not None:    
-    print("company:  " + usr.company)
+dct = {'user': usr.login,
+       'fullname': usr.name,
+       'location': usr.location,
+       'company': usr.company
+       }
+
+print ("dictionary is " + json.dumps(dct))
+
 
 
