f6cf8a453e09c2c746aceba0002649ac73441221
Stephen Barrett <stephen.barrett@tcd.ie>
2021-11-08 11:23:38 +0000

    Start of project - simple hellow world

---
+++ README.md
@@ -0,0 +1,5 @@
+This repository provides a python equivalent to my repositories that demonstrate github access in haskell. 
+
+There is an accompanying recorded lecture.
+
+Stephen
---
+++ script.py
@@ -0,0 +1,5 @@
+
+# a script to do python based access to the github api
+# let's start with a simple hellow world app to get outselves going.
+print("Demonstration python based github api access");
+
