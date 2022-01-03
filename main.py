8aba731330a4cf9bf5dd68b641934332797b1578
ara-gone <72136410+ara-gone@users.noreply.github.com>
2022-01-03 23:52:27 +0000

    Added .csv compatability and gitignore

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from github import Github
+from config import *
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
-atk = "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" # insert own access token
+atk = get_access_token() # insert own access token
 g = Github(atk)
 
 user = g.get_user()
 print('Loading repos for... ' + user.name)
 
 repos = g.get_user().get_repos()
+df = pd.DataFrame()
 
 print('-------------REPOSITORIES---------------')
 for r in repos:
     print(r.name)
+    prs = r.get_pulls(state='all')
+    df = df.append({
+        'ID': r.id,
+        'Name': r.name,
+        'Language': r.language,
+        'Stars': r.stargazers_count,
+        'Watchers': r.subscribers_count,
+        'Pull_Requests': prs.totalCount
+    }, ignore_index=True)
+
+df.to_csv('../repos.csv', sep=',', encoding='utf-8', index=True)
