b2c784c3a894468a2ac14b7e280af4ebc8b814d0
ara-gone <72136410+ara-gone@users.noreply.github.com>
2022-01-03 22:32:53 +0000

    Adding pandas + numpy imports

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from github import Github
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-atk = "" # delete later!
+atk = "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" # insert own access token
 g = Github(atk)
 
 user = g.get_user()
