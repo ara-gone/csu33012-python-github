acb5185b2078e6f4ba2d951a7341ca8821ff5d2a
ara-gone <72136410+ara-gone@users.noreply.github.com>
2022-01-04 01:08:26 +0000

    Added exceptions imports

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from github import Github
+from github import Github, RateLimitExceededException, BadCredentialsException
 from config import *
 import pandas as pd
 import numpy as np
@@ -16,6 +16,7 @@ df_commits = pd.DataFrame()
 print('-------------REPOSITORIES---------------')
 for r in repos:
     repo_commits = r.get_commits()
+    # full name is username/reponame format, needed for pygithub lookup
     print(r.full_name + ', with commits -> ' + str(repo_commits.totalCount))
     prs = r.get_pulls(state='all')
 
@@ -37,5 +38,6 @@ while repo_name != 'quit':
             'Pull_Requests': prs.totalCount
         }, ignore_index=True)
         df.to_csv('../repos.csv', sep=',', encoding='utf-8', index=True)
+        # df.to_csv('repos.csv', mode='a', header=False)
     else:
         print('Successfully quit program.')
